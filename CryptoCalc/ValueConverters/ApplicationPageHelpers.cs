f57e5c430d63c6d251a5057c388733172ba3e37b
danderle <danderle86@gmail.com>
2020-05-13 08:11:39 +0200

    deleted thew ApplicationPageValueConverter.cs and created the ApplicationPageHelpers.cs for switching an ApplicationPage to a BasePage and vice versa and loading an specific view model

---
+++ CryptoCalc/ValueConverters/ApplicationPageHelpers.cs
@@ -0,0 +1,50 @@
+﻿using CryptoCalc.Core;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Converts the <see cref="ApplicationPage"/> to an actual view/page
+    /// </summary>
+    public static class ApplicationPageHelpers
+    {
+        /// <summary>
+        /// Takes a <see cref="ApplicationPage"/> and a view model, if any, and creates the desired page
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="viewModel"></param>
+        /// <returns></returns>
+        public static BasePage ToBasePage(this ApplicationPage page, object viewModel = null)
+        {
+            // Find the appropriate page
+            switch (page)
+            {
+                case ApplicationPage.Hash:
+                case ApplicationPage.SymmetricCiphers:
+                case ApplicationPage.AsymmetricCiphers:
+                    return new HashPage(viewModel as HashViewModel);
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns></returns>
+        public static ApplicationPage ToApplicationPage(this BasePage page)
+        {
+            if (page is HashPage)
+                return ApplicationPage.Hash;
+
+            //Alert developer
+            Debugger.Break();
+            return default(ApplicationPage);
+        }
+    }
+}
