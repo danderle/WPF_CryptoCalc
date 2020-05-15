3147a1077c1a5d67c0197624283452f254497089
danderle <danderle86@gmail.com>
2020-05-15 08:35:51 +0200

    Added a HashItemListControl which will display all the HashItemControl all the hashing algorithims

---
+++ CryptoCalc/Controls/HashItemListControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for HashItemListControl.xaml
+    /// </summary>
+    public partial class HashItemListControl : UserControl
+    {
+        #region Constructor
+
+        public HashItemListControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
