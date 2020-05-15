722583b8082b148df5a05ba35e425e4a1bd0c918
danderle <danderle86@gmail.com>
2020-05-15 08:30:31 +0200

    A value converter for Convertíng the <see cref="DataHashFormat"/>enum value to a string value

---
+++ CryptoCalc/ValueConverters/DataHashFormatEnumToStringValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using CryptoCalc.Core;
+using System;
+using System.Globalization;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Converts the <see cref="DataHashFormat"/>enum value to a string value
+    /// </summary>
+    public class DataHashFormatEnumToString : BaseValueConverter<DataHashFormatEnumToString>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((int)(DataHashFormat)value).ToString();
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Enum.Parse(typeof(DataHashFormat), value.ToString());
+        }
+    }
+}
