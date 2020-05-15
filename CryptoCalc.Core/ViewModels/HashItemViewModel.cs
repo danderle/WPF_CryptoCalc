7c81d95ac142ea0c8b3a265adee3f28b1a0a4153
danderle <danderle86@gmail.com>
2020-05-15 08:34:21 +0200

    The HashItemViewModel for the HashItemControl to bind to

---
+++ CryptoCalc.Core/ViewModels/HashItemViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoCalc.Core
+{
+    public class HashItemViewModel : BaseViewModel
+    {
+
+        #region Properties
+
+        public bool IsChecked { get; set; }
+
+        public string HashName { get; set; }
+
+        public string HashValue { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public HashItemViewModel()
+        {
+        } 
+
+        #endregion
+    }
+}
