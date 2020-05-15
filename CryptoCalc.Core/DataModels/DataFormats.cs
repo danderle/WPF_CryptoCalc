236bfe0f9f37dc6b93416d789d8a404719452304
danderle <danderle86@gmail.com>
2020-05-15 08:29:53 +0200

    DataFormat options as enum values

---
+++ CryptoCalc.Core/DataModels/DataFormats.cs
@@ -0,0 +1,23 @@
+﻿namespace CryptoCalc.Core
+{
+    /// <summary>
+    /// Data format options for hashing
+    /// </summary>
+    public enum DataHashFormat
+    {
+        /// <summary>
+        /// Hash a file
+        /// </summary>
+        File = 0,
+
+        /// <summary>
+        /// Hash a string
+        /// </summary>
+        TextString,
+
+        /// <summary>
+        /// Hash a hex string
+        /// </summary>
+        HexString,
+    }
+}
