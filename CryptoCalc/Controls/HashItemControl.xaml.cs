bb330c67865b57c886849420d84c97d4f12e86f4
danderle <danderle86@gmail.com>
2020-05-15 08:33:54 +0200

    Added the HashItemControl which is one Hash algorithim

---
+++ CryptoCalc/Controls/HashItemControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for HashItemControl.xaml
+    /// </summary>
+    public partial class HashItemControl : UserControl
+    {
+        #region Constructor
+
+        public HashItemControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
