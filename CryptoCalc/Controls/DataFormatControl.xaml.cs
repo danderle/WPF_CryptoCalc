a22a63643c9265de585d7ac41de48846ea190de9
danderle <danderle86@gmail.com>
2020-05-15 08:28:31 +0200

    Created a DataFormatControl, which will prompt the user to setup the Hashing options and the data to hash

---
+++ CryptoCalc/Controls/DataFormatControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for DataFormat.xaml
+    /// </summary>
+    public partial class DataFormatControl : UserControl
+    {
+        #region Constructor
+
+        public DataFormatControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
