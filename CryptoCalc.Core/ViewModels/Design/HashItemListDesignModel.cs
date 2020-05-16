39e6bda430708a2b86fe7a13dbbeaaf52bc140d8
danderle <danderle86@gmail.com>
2020-05-16 08:42:23 +0200

    added a HashItemListDesignModel.cs and HashItemListViewModel.cs

---
+++ CryptoCalc.Core/ViewModels/Design/HashItemListDesignModel.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace CryptoCalc.Core
+{
+    public class HashItemListDesignModel : HashItemListViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static HashItemListDesignModel Instance => new HashItemListDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public HashItemListDesignModel()
+        {
+            Items = new List<HashItemViewModel>
+            {
+                new HashItemViewModel
+                {
+                    IsChecked = true,
+                    HashName = "SHA1",
+                    HashValue = "444ffff444f4f4aaa3",
+                },
+                new HashItemViewModel
+                {
+                    IsChecked = true,
+                    HashName = "SHA2",
+                    HashValue = "juzjzuesf3444f4f4aaa3",
+                },
+                new HashItemViewModel
+                {
+                    IsChecked = true,
+                    HashName = "SHA3",
+                    HashValue = "wfew4t564uh",
+                },
+                new HashItemViewModel
+                {
+                    IsChecked = true,
+                    HashName = "CRC32",
+                    HashValue = "7ijhefg234",
+                },
+                new HashItemViewModel
+                {
+                    IsChecked = true,
+                    HashName = "MD2",
+                    HashValue = "g65h",
+                },
+                new HashItemViewModel
+                {
+                    IsChecked = true,
+                    HashName = "MD5",
+                    HashValue = "876i8kjhg",
+                },
+
+            };
+        } 
+
+        #endregion
+    }
+}
---
+++ CryptoCalc.Core/ViewModels/HashItemListViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace CryptoCalc.Core
+{
+    public class HashItemListViewModel : BaseViewModel
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// A list of the <see cref="HashItemViewModel"/>
+        /// </summary>
+        public List<HashItemViewModel> Items { get; set; } = new List<HashItemViewModel>();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public HashItemListViewModel()
+        {
+        } 
+
+        #endregion
+    }
+}
