148de1013d4932ac6625351a56aa5c7564ce1d71
danderle <danderle86@gmail.com>
2020-05-16 08:40:50 +0200

    added comments to the HashItemViewModel

--- CryptoCalc.Core/ViewModels/HashItemViewModel.cs
+++ CryptoCalc.Core/ViewModels/HashItemViewModel.cs
@@ -1,18 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CryptoCalc.Core
+﻿namespace CryptoCalc.Core
 {
     public class HashItemViewModel : BaseViewModel
     {
 
         #region Properties
 
+        /// <summary>
+        /// True if the hash algorithim is checked
+        /// </summary>
         public bool IsChecked { get; set; }
 
+        /// <summary>
+        /// The hash algorithims name
+        /// </summary>
         public string HashName { get; set; }
 
+        /// <summary>
+        /// The hash value
+        /// </summary>
         public string HashValue { get; set; }
 
         #endregion
