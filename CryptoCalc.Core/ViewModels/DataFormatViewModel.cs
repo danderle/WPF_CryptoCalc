4eb7c51960eab2cacc21851300b03cdb14e343e5
danderle <danderle86@gmail.com>
2020-05-16 08:41:17 +0200

    added comments to the DataFormatViewModel

--- CryptoCalc.Core/ViewModels/DataFormatViewModel.cs
+++ CryptoCalc.Core/ViewModels/DataFormatViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace CryptoCalc.Core
 {
@@ -9,17 +9,39 @@ namespace CryptoCalc.Core
 
         #region Properties
 
+        /// <summary>
+        /// Keyed-Hash Message Authentication Code
+        /// </summary>
         public bool HmacChecked { get; set; } = false;
 
+        /// <summary>
+        /// The data to be hashed <see cref="DataHashFormat"/> for hash data format options
+        /// </summary>
         public string Data { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The Hmac key
+        /// </summary>
         public string Key { get; set; } = string.Empty;
 
+        /// <summary>
+        /// List for holding the key format options
+        /// </summary>
         public List<string> KeyFormat { get; set; } = new List<string>();
 
+        /// <summary>
+        /// Holds the dat format options
+        /// </summary>
         public List<string> DataFormat { get; set; } = new List<string>();
 
+        /// <summary>
+        /// Currently selected data format
+        /// </summary>
         public DataHashFormat DataFormatSelected { get; set; } = DataHashFormat.File;
+
+        /// <summary>
+        /// Currently selected key format
+        /// </summary>
         public DataHashFormat KeyFormatSelected { get; set; } = DataHashFormat.TextString;
 
         #endregion
@@ -31,10 +53,8 @@ namespace CryptoCalc.Core
         /// </summary>
         public DataFormatViewModel()
         {
-            DataFormat.Add(DataHashFormat.File.ToString());
-            DataFormat.Add(DataHashFormat.TextString.ToString());
-            DataFormat.Add(DataHashFormat.HexString.ToString());
-
+            // Adds the formats to the lists
+            DataFormat = Enum.GetValues(typeof(DataHashFormat)).Cast<DataHashFormat>().Select(t => t.ToString()).ToList();
             KeyFormat.Add(DataHashFormat.TextString.ToString());
             KeyFormat.Add(DataHashFormat.HexString.ToString());
         } 
