06ba25e95617b2665895915af53b5874586c4c5f
danderle <danderle86@gmail.com>
2020-05-16 08:43:05 +0200

    added a DataFormatDesignModel

---
+++ CryptoCalc.Core/ViewModels/Design/DataFormatDesignModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace CryptoCalc.Core
+{
+    public class DataFormatDesignModel : DataFormatViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static DataFormatDesignModel Instance => new DataFormatDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public DataFormatDesignModel()
+        {
+            HmacChecked = false;
+            Data = "Data to be hashed";
+            Key = "some kind of hash key";
+
+            KeyFormat.Add(DataHashFormat.TextString.ToString());
+            KeyFormat.Add(DataHashFormat.HexString.ToString());
+            DataFormat.Add(DataHashFormat.File.ToString());
+            DataFormat.Add(DataHashFormat.TextString.ToString());
+            DataFormat.Add(DataHashFormat.HexString.ToString());
+
+            DataFormatSelected = DataHashFormat.File;
+            KeyFormatSelected = DataHashFormat.TextString;
+        } 
+
+        #endregion
+    }
+}
