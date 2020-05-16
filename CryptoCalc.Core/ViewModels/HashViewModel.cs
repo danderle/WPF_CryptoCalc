a670f54349e177f44c6dbef1eb3ad7e1aa7e1f5e
danderle <danderle86@gmail.com>
2020-05-16 08:43:53 +0200

    added comments to HashViewModel

--- CryptoCalc.Core/ViewModels/HashViewModel.cs
+++ CryptoCalc.Core/ViewModels/HashViewModel.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CryptoCalc.Core
+﻿namespace CryptoCalc.Core
 {
     public class HashViewModel : BaseViewModel
     {
 
         #region Properties
 
+        /// <summary>
+        /// The view model to setup the hashing process
+        /// </summary>
         public DataFormatViewModel DataFormatSetup { get; set; } = new DataFormatViewModel();
+
 
         #endregion
 
