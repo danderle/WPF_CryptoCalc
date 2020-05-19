72163b447d2f0a64360800c93ba3a38aa5966190
danderle <danderle86@gmail.com>
2020-05-19 08:10:00 +0200

    added the HashAlgorithim.cs enums for representing all hash algorithims

---
+++ CryptoCalc.Core/DataModels/HashAlgorithim.cs
@@ -0,0 +1,21 @@
+﻿namespace CryptoCalc.Core
+{
+    /// <summary>
+    /// Represents all the available hashing algorithims
+    /// </summary>
+    public enum HashAlgorithim
+    {
+        MD5 = 0,
+        MD4,
+        SHA1,
+        SHA256,
+        SHA384,
+        SHA512,
+        RIPEMD160,
+        PANAMA,
+        TIGER,
+        MD2,
+        ADLER32,
+        CRC32,
+    }
+}
