dc31756f267ab402db72a69a5a272539c8b315ed
danderle <danderle86@gmail.com>
2020-05-19 08:16:19 +0200

    added default values when creating the HashItemViewModel

--- CryptoCalc.Core/ViewModels/HashItemViewModel.cs
+++ CryptoCalc.Core/ViewModels/HashItemViewModel.cs
@@ -1,14 +1,30 @@
-﻿namespace CryptoCalc.Core
+﻿using CryptoCalc.Core.Models;
+using System;
+using System.Text;
+using System.ComponentModel;
+
+namespace CryptoCalc.Core
 {
+    /// <summary>
+    /// The view model for the <see cref="HashItemControl.xaml"/>
+    /// </summary>
     public class HashItemViewModel : BaseViewModel
     {
+        #region Private Fields
+
+        /// <summary>
+        /// The hash algorithim to use when calculating the hash value
+        /// </summary>
+        private HashAlgorithim hashAlgorithim => (HashAlgorithim)Enum.Parse(typeof(HashAlgorithim), HashName);
+
+        #endregion
 
         #region Properties
 
         /// <summary>
         /// True if the hash algorithim is checked
         /// </summary>
-        public bool IsChecked { get; set; }
+        public bool IsChecked { get; set; } = false;
 
         /// <summary>
         /// The hash algorithims name
@@ -18,7 +34,7 @@
         /// <summary>
         /// The hash value
         /// </summary>
-        public string HashValue { get; set; }
+        public string HashValue { get; set; } = string.Empty;
 
         #endregion
 
@@ -29,7 +45,17 @@
         /// </summary>
         public HashItemViewModel()
         {
-        } 
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void CalculateHash(byte[] data)
+        {
+            var value = Hash.Compute(hashAlgorithim, data);
+            HashValue = BitConverter.ToString(value).Replace("-", string.Empty);
+        }
 
         #endregion
     }
