d13b2a04ead33e54ad46fe489e1b2f6061e01282
danderle <danderle86@gmail.com>
2020-05-25 08:57:49 +0200

    in the DataFormatControl.xaml added a binding for the Hmac checkbox and in the .cs file a dependency property if Hmac is selected

--- CryptoCalc/Controls/DataFormatControl.xaml.cs
+++ CryptoCalc/Controls/DataFormatControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace CryptoCalc
 {
@@ -7,6 +10,25 @@ namespace CryptoCalc
     /// </summary>
     public partial class DataFormatControl : UserControl
     {
+        #region Dependency Properties
+
+        /// <summary>
+        /// Dependency property for determining if the hmac checkbox is selected
+        /// </summary>
+        public bool HmacSelected
+        {
+            get => (bool)GetValue(HmacSelectedProperty);
+            set => SetValue(HmacSelectedProperty, value);
+        }
+
+        /// <summary>
+        /// Registers <see cref="HmacSelected"/> as a dependency property
+        /// </summary>
+        public static readonly DependencyProperty HmacSelectedProperty =
+            DependencyProperty.Register(nameof(HmacSelected), typeof(bool), typeof(DataFormatControl), new PropertyMetadata(false));
+
+        #endregion
+
         #region Constructor
 
         public DataFormatControl()
