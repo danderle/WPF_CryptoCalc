a1b423c548bb2fbd688100cf4ab9e0210c5228b3
danderle <danderle86@gmail.com>
2020-05-25 09:06:33 +0200

    added a bool to visibility converter

---
+++ CryptoCalc/ValueConverters/BoolToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Converts a bool to a visibiliy
+    /// </summary>
+    public class BoolToVisibility : BaseValueConverter<BoolToVisibility>
+    {
+        public override object Convert(object value, Type targetType = null, object parameter = null, CultureInfo culture = null)
+        {
+            bool retVal = (bool)value;
+            if(parameter != null)
+            {
+                retVal &= (bool)parameter;
+            }
+            return retVal ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
