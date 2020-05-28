e37e8145ef7d4822549dc0b744601e6924bbc6a0
danderle <danderle86@gmail.com>
2020-05-28 07:16:08 +0200

    added the IUIManager.cs to handle UI interaction in the application

---
+++ CryptoCalc.Core/IoC/Interfaces/IUIManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoCalc.Core
+{
+    /// <summary>
+    /// The UI manager that handles any UI interaction in the application
+    /// </summary>
+    public interface IUIManager
+    {
+        /// <summary>
+        /// Displays a single message box to the user
+        /// </summary>
+        /// <param name="viewModel">The view model</param>
+        /// <returns></returns>
+        Task ShowMessage(MessageBoxDialogViewModel viewModel);
+    }
+}
