f8c7e8e3f24c8760640c657d8ea3a48800018524
danderle <danderle86@gmail.com>
2020-05-28 07:20:38 +0200

    added a DialogMessageBox

---
+++ CryptoCalc/Dialogs/DialogMessageBox.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for DialogMessageBox.xaml
+    /// </summary>
+    public partial class DialogMessageBox : BaseDialogUserControl
+    {
+        #region Constructor
+
+        public DialogMessageBox()
+        {
+            InitializeComponent();
+        } 
+
+        #endregion
+    }
+}
