d0dfa4fbda927113414f2363b5b9d267bbeb892d
danderle <danderle86@gmail.com>
2020-05-28 07:17:26 +0200

    added a base dialog view model

---
+++ CryptoCalc.Core/ViewModels/Dialogs/BaseDialogViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CryptoCalc.Core
+{
+    /// <summary>
+    /// A base view model for any dialogs
+    /// </summary>
+    public class BaseDialogViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The dialog title text
+        /// </summary>
+        public string Title { get; set; }
+    }
+}
