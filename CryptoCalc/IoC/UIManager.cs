4c09bb2c1cf9137fa0a223c5c157fe6fa4d4a377
danderle <danderle86@gmail.com>
2020-05-28 07:21:33 +0200

    added a UIManager to display a single message box

---
+++ CryptoCalc/IoC/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using CryptoCalc.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// The applications implementation of the <see cref="IUIManager"/>
+    /// </summary>
+    public class UIManager : IUIManager
+    {
+        /// <summary>
+        /// Displays a single message box to the user
+        /// </summary>
+        /// <param name="viewModel">The view model</param>
+        /// <returns></returns>
+        public Task ShowMessage(MessageBoxDialogViewModel viewModel)
+        {
+            return new DialogMessageBox().ShowMessage(viewModel);
+        }
+    }
+}
