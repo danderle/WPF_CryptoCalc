de64bbb383849c25ee61f5f3a8183a0de279d8b2
danderle <danderle86@gmail.com>
2020-05-28 07:19:53 +0200

    added the dialog window

---
+++ CryptoCalc/DialogWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for DialogWindow.xaml
+    /// </summary>
+    public partial class DialogWindow : Window
+    {
+        #region Private Fields
+
+        /// <summary>
+        /// The view model for this window
+        /// </summary>
+        private DialogWindowViewModel viewModel;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// the view model for this window
+        /// </summary>
+        public DialogWindowViewModel ViewModel
+        {
+            get => viewModel;
+            set
+            {
+                //Set the new view model
+                viewModel = value;
+
+                //update the data context
+                DataContext = viewModel;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public DialogWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new DialogWindowViewModel(this);
+        } 
+
+        #endregion
+    }
+}
