a1f21391f27caa1f79398b7bc42dd15f7280e937
danderle <danderle86@gmail.com>
2020-05-28 07:20:54 +0200

    added a MessageBoxDialogDesignModel

---
+++ CryptoCalc.Core/ViewModels/Dialogs/Design/MessageBoxDialogDesignModel.cs
@@ -0,0 +1,28 @@
+﻿namespace CryptoCalc.Core
+{
+    public class MessageBoxDialogDesignModel : MessageBoxDialogViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static MessageBoxDialogDesignModel Instance => new MessageBoxDialogDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public MessageBoxDialogDesignModel()
+        {
+            Message = "Design Time Message Box";
+            OkText = "The ok Button";
+            Title = "Desgin Time";
+        } 
+
+        #endregion
+    }
+}
