172aac87185de81f41b4fc909de96a1e94c7c7d6
danderle <danderle86@gmail.com>
2020-05-28 07:22:09 +0200

    added the DialogeWindowViewModel

---
+++ CryptoCalc/ViewModels/DialogWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CryptoCalc.Core;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// The view model for the custom window
+    /// </summary>
+    public class DialogWindowViewModel : WindowViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The title of this dialog window
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The content to host inside the dialog window
+        /// </summary>
+        public Control Content { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public DialogWindowViewModel(Window _window) : base(_window) 
+        {
+            //Set minimum size of window
+            MinimumHeight = 100;
+            MinimumWidth = 250;
+        }
+        
+        #endregion
+
+    }
+}
