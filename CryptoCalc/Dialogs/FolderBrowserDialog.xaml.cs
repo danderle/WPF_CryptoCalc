e444c40ce217d6f482c8dbe86e55fe56861b9f97
danderle <danderle86@gmail.com>
2020-05-30 11:03:33 +0200

    added a Folder FolderBrowserDialog control and implemented the TreeViewControl

---
+++ CryptoCalc/Dialogs/FolderBrowserDialog.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for FolderBrowserDialog.xaml
+    /// </summary>
+    public partial class FolderBrowserDialog : BaseDialogUserControl
+    {
+        #region Constructor
+
+        public FolderBrowserDialog()
+        {
+            InitializeComponent();
+        } 
+
+        #endregion
+    }
+}
