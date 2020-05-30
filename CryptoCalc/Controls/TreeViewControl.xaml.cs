d117ba69c430c40903702781cdac0e89c39f526a
danderle <danderle86@gmail.com>
2020-05-30 11:02:32 +0200

    added some images and a TreeViewControl

---
+++ CryptoCalc/Controls/TreeViewControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for TreeViewControl.xaml
+    /// </summary>
+    public partial class TreeViewControl : UserControl
+    {
+        #region Constructor
+
+        public TreeViewControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
