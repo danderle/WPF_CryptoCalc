760f6c81f3c96f679c45f4821eeaca6b4449403b
danderle <danderle86@gmail.com>
2020-06-03 11:28:56 +0200

    added converter to Convert the <see cref="TreeItemType"/>enum value to an image path

---
+++ CryptoCalc/ValueConverters/TreeItemTypeToImagePathConverter.cs
@@ -0,0 +1,40 @@
+﻿using CryptoCalc.Core;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Converts the <see cref="TreeItemType"/>enum value to an image path
+    /// </summary>
+    public class TreeItemTypeToImagePath : BaseValueConverter<TreeItemTypeToImagePath>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string imagePath = "\\Resources\\Images\\";
+            switch ((TreeItemType)value)
+            {
+                case TreeItemType.LogicalDrive:
+                    imagePath += "drive.png";
+                    break;
+                case TreeItemType.Directory:
+                    imagePath += "folder.png";
+                    break;
+                case TreeItemType.File:
+                    imagePath += "file.png";
+                    break;
+                default:
+                    //Some type of error
+                    Debugger.Break();
+                    break;
+            }
+            return imagePath;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (DataHashFormat)value;
+        }
+    }
+}
