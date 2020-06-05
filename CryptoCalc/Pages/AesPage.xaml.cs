dc27f23279374be57c77ab3b1464bcc2104e76aa
danderle <danderle86@gmail.com>
2020-06-05 11:27:31 +0200

    added the AesPage.xaml and AesPage.xaml.cs for the aes en- and decryption

---
+++ CryptoCalc/Pages/AesPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using CryptoCalc.Core;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for AesPage.xaml
+    /// </summary>
+    public partial class AesPage : BasePage<AesViewModel>
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public AesPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Constructor with a specific view model
+        /// </summary>
+        public AesPage(AesViewModel specificViewModel) : base(specificViewModel)
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
