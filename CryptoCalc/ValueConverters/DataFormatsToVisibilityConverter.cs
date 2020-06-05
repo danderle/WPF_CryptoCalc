f990b7e75d4c034e8e3dd0465031e14c6524305f
danderle <danderle86@gmail.com>
2020-06-05 11:32:01 +0200

    added a DataFormatsToVisibility converter

---
+++ CryptoCalc/ValueConverters/DataFormatsToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using CryptoCalc.Core;
+using System;
+using System.Globalization;
+using System.Windows;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Converts a <see cref="Format"/> to a visibiliy
+    /// </summary>
+    public class DataFormatsToVisibility : BaseValueConverter<DataFormatsToVisibility>
+    {
+        public override object Convert(object value, Type targetType = null, object parameter = null, CultureInfo culture = null)
+        {
+            var retVal = ((Core.Format)value).ToString() == (string)parameter;
+            return retVal ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
