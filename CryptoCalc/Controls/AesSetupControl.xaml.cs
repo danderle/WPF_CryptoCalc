1fa46a00d80d57452b9ad7810b24658b0d0c6b0c
danderle <danderle86@gmail.com>
2020-06-06 07:44:03 +0200

    added the AesSetupControl and CipherInputControl and implmented them into the AesPage

---
+++ CryptoCalc/Controls/AesSetupControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for AesSetupControl.xaml
+    /// </summary>
+    public partial class AesSetupControl : UserControl
+    {
+        #region Constructor
+
+        public AesSetupControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
---
+++ CryptoCalc/Controls/CipherInputControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for CipherInputControl.xaml
+    /// </summary>
+    public partial class CipherInputControl : UserControl
+    {
+        #region Constructor
+
+        public CipherInputControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
