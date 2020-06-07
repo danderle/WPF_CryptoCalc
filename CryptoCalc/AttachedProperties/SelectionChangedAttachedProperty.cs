6fc3edff0b2988fb058af9ffb053e2d19f7516e9
danderle <danderle86@gmail.com>
2020-06-07 08:21:40 +0200

    added a selection changed attached property

---
+++ CryptoCalc/AttachedProperties/SelectionChangedAttachedProperty.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Calls a command method when the selection of a combobox is changed
+    /// </summary>
+    public class SelectionChangedProperty : BaseAttachedProperty<SelectionChangedProperty, ICommand>
+    {
+        /// <summary>
+        /// Fires when a new command is set
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            comboBox.SelectionChanged += (ss, ee) =>
+            {
+                ICommand command = GetValue(comboBox);
+
+                command.Execute(e);
+            };
+        }
+    }
+}
