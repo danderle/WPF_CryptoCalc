15c0eddad9483b5381f5d220c997ac26b6b2526a
danderle <danderle86@gmail.com>
2020-06-07 08:20:07 +0200

    added a enum for the symmetric ciphers

---
+++ CryptoCalc.Core/DataModels/SymmetricCipherAlgorithim.cs
@@ -0,0 +1,13 @@
+﻿namespace CryptoCalc.Core
+{
+    /// <summary>
+    /// Represents all the available symmetric cipher algorithims
+    /// </summary>
+    public enum SymmetricCipherAlgorithim
+    {
+        Aes = 0,
+        DES,
+        RC2,
+        TripleDES,
+    }
+}
