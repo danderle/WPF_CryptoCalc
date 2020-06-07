0c037cb4fb4fd5192cbb3ed39c40646e40978074
danderle <danderle86@gmail.com>
2020-06-07 08:23:01 +0200

    refactored the symmetricCiphers class for encryptiong and decrypting and setting up the cipher options

--- CryptoCalc.Core/Models/SymmetricCiphers/SymmetricCipher.cs
+++ CryptoCalc.Core/Models/SymmetricCiphers/SymmetricCipher.cs
@@ -11,7 +11,10 @@ namespace CryptoCalc.Core
     {
         #region Public Properties
 
-        public static Aes AesCrypt { get; set; } 
+        /// <summary>
+        /// The symmetric cipher algorithim to use for en-/decryption 
+        /// </summary>
+        public static SymmetricAlgorithm Algorithim { get; set; }
 
         #endregion
 
@@ -22,199 +25,183 @@ namespace CryptoCalc.Core
         /// </summary>
         static SymmetricCipher()
         {
-            AesCrypt = Aes.Create();
-            var bsize = AesCrypt.BlockSize;
-            var fsize = AesCrypt.FeedbackSize;
-            var ksize = AesCrypt.KeySize;
-            var lsize = AesCrypt.LegalBlockSizes;
-            var msize = AesCrypt.LegalKeySizes;
         }
 
         #endregion
 
         #region Public Methods
 
-        public static byte[] AesEncrypt(byte[] password, int keySize, string plainText)
+        /// <summary>
+        /// Encrypt a plain string
+        /// </summary>
+        /// <param name="algorithim">The symmetric algorithim to use</param>
+        /// <param name="password">The password to hash and create the secret key and iv with</param>
+        /// <param name="plain">the plain string to encrypt</param>
+        /// <returns>Encrypted bytes</returns>
+        public static byte[] Encrypt(SymmetricAlgorithm algorithim, byte[] password, string plain)
         {
-            SetKeyAndIV(password, keySize);
-            return Encrypt(plainText, AesCrypt.Key, AesCrypt.IV);
+            Algorithim = algorithim;
+            SetKeyAndIV(password);
+            CheckCipherSetup(plain);
+            return Encrypt(plain);
         }
 
-        public static byte[] AesEncrypt(byte[] password, int keySize, byte[] plainText)
+        /// <summary>
+        /// Encrypt plain bytes
+        /// </summary>
+        /// <param name="algorithim">The symmetric algorithim to use</param>
+        /// <param name="password">The password to hash and create the secret key and iv with</param>
+        /// <param name="plain">the plain bytes to encrypt</param>
+        /// <returns>Encrypted bytes</returns>
+        public static byte[] Encrypt(SymmetricAlgorithm algorithim, byte[] password, byte[] plain)
         {
-            SetKeyAndIV(password, keySize);
-            return Encrypt(plainText, AesCrypt.Key, AesCrypt.IV);
+            Algorithim = algorithim;
+            SetKeyAndIV(password);
+            CheckCipherSetup(plain);
+            return Encrypt(plain);
         }
 
-        public static string AesDecryptToText(byte[] password, int keySize, byte[] encrypted)
+        /// <summary>
+        /// Decrypt to plain string
+        /// </summary>
+        /// <param name="algorithim">The symmetric algorithim to use</param>
+        /// <param name="password">The password to hash and create the secret key and iv with</param>
+        /// <param name="encrypted">The encrypted bytes</param>
+        /// <returns>decrypted string</returns>
+        public static string DecryptToText(SymmetricAlgorithm algorithim, byte[] password, byte[] encrypted)
         {
-            SetKeyAndIV(password, keySize);
-            return DecryptToText(encrypted, AesCrypt.Key, AesCrypt.IV);
+            Algorithim = algorithim;
+            SetKeyAndIV(password);
+            CheckCipherSetup(encrypted);
+            return DecryptToText(encrypted);
         }
 
-        public static byte[] AesDecryptToByte(byte[] password, int keySize, byte[] encrypted)
+        /// <summary>
+        /// Decrypt to plain string
+        /// </summary>
+        /// <param name="algorithim">The symmetric algorithim to use</param>
+        /// <param name="password">The password to hash and create the secret key and iv with</param>
+        /// <param name="encrypted">The encrypted bytes</param>
+        /// <returns>decrypted bytes</returns>
+        public static byte[] DecryptToByte(SymmetricAlgorithm algorithim, byte[] password, byte[] encrypted)
         {
-            SetKeyAndIV(password, keySize);
-            return DecryptToByte(encrypted, AesCrypt.Key, AesCrypt.IV);
+            Algorithim = algorithim;
+            SetKeyAndIV(password);
+            CheckCipherSetup(encrypted);
+            return DecryptToByte(encrypted);
+        }
+
+        /// <summary>
+        /// Gets a new symmetric algorithim object
+        /// </summary>
+        /// <param name="algorithim">the type of symmetric algorithim toi return</param>
+        /// <returns>The symmetric algorithim object</returns>
+        public static SymmetricAlgorithm GetAlgorithim(SymmetricCipherAlgorithim algorithim)
+        {
+            switch(algorithim)
+            {
+                case SymmetricCipherAlgorithim.Aes:
+                    return Aes.Create();
+                case SymmetricCipherAlgorithim.DES:
+                    return DES.Create();
+                case SymmetricCipherAlgorithim.TripleDES:
+                    return TripleDES.Create();
+                case SymmetricCipherAlgorithim.RC2:
+                    return RC2.Create();
+                default:
+                    Debugger.Break();
+                    return null;
+            }
         }
 
         #endregion
 
         #region Private Methods
 
-        private static string DecryptToText(byte[] encrypted, byte[] key, byte[] iv)
+        /// <summary>
+        /// Decrypt to plain string
+        /// </summary>
+        /// <param name="encrypted">the encrypted bytes</param>
+        /// <returns>the plain string</returns>
+        private static string DecryptToText(byte[] encrypted)
         {
-            // Check arguments.
-            if (encrypted == null || encrypted.Length <= 0)
-                throw new ArgumentNullException("cipherText");
-            if (key == null || key.Length <= 0)
-                throw new ArgumentNullException("key");
-            if (iv == null || iv.Length <= 0)
-                throw new ArgumentNullException("iV");
-
             // Declare the string used to hold
             // the decrypted text.
             string plaintext = null;
 
-            // Create an Aes object
-            // with the specified key and IV.
-            using (Aes aesAlg = Aes.Create())
+            // Create a decryptor to perform the stream transform.
+            ICryptoTransform decryptor = Algorithim.CreateDecryptor(Algorithim.Key, Algorithim.IV);
+
+            // Create the streams used for decryption.
+            using (MemoryStream msDecrypt = new MemoryStream(encrypted))
             {
-                aesAlg.Key = key;
-                aesAlg.IV = iv;
-
-                // Create a decryptor to perform the stream transform.
-                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
-
-                // Create the streams used for decryption.
-                using (MemoryStream msDecrypt = new MemoryStream(encrypted))
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
                 {
-                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
                     {
-                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
-                        {
-                            // Read the decrypted bytes from the decrypting stream
-                            // and place them in a string.
-                            plaintext = srDecrypt.ReadToEnd();
-                        }
+                        // Read the decrypted bytes from the decrypting stream
+                        // and place them in a string.
+                        plaintext = srDecrypt.ReadToEnd();
                     }
                 }
             }
 
             return plaintext;
         }
-        private static byte[] DecryptToByte(byte[] encrypted, byte[] key, byte[] iv)
-        {
-            // Check arguments.
-            if (encrypted == null || encrypted.Length <= 0)
-                throw new ArgumentNullException("cipherText");
-            if (key == null || key.Length <= 0)
-                throw new ArgumentNullException("key");
-            if (iv == null || iv.Length <= 0)
-                throw new ArgumentNullException("iV");
 
+        /// <summary>
+        /// Decrypt to plain bytes
+        /// </summary>
+        /// <param name="encrypted">the encrypted bytes</param>
+        /// <returns>the plain bytes</returns>
+        private static byte[] DecryptToByte(byte[] encrypted)
+        {
             // Declare the string used to hold
             // the decrypted text.
             byte[] plainBytes = null;
 
-            // Create an Aes object
-            // with the specified key and IV.
-            using (Aes aesAlg = Aes.Create())
+            // Create a decryptor to perform the stream transform.
+            ICryptoTransform decryptor = Algorithim.CreateDecryptor(Algorithim.Key, Algorithim.IV);
+
+            // Create the streams used for decryption.
+            using (MemoryStream msDecrypt = new MemoryStream())
             {
-                aesAlg.Key = key;
-                aesAlg.IV = iv;
-
-                // Create a decryptor to perform the stream transform.
-                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
-
-                // Create the streams used for decryption.
-                using (MemoryStream msDecrypt = new MemoryStream())
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Write))
                 {
-                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Write))
+                    using (var binWriter = new BinaryWriter(csDecrypt))
                     {
-                        using(var binWriter = new BinaryWriter(csDecrypt))
-                        {
-                            binWriter.Write(encrypted);
-                        }
-                        plainBytes = msDecrypt.ToArray();
+                        binWriter.Write(encrypted);
                     }
+                    plainBytes = msDecrypt.ToArray();
                 }
             }
+
             return plainBytes;
         }
 
-        private static byte[] Encrypt(string plain, byte[] key, byte[] iv)
+        /// <summary>
+        /// Encrypt a plain string
+        /// </summary>
+        /// <param name="plain">the plain string</param>
+        /// <returns>encrypted bytes</returns>
+        private static byte[] Encrypt(string plain)
         {
-            // Check arguments.
-            if (plain == null || plain.Length <= 0)
-                throw new ArgumentNullException("plain");
-            if (key == null || key.Length <= 0)
-                throw new ArgumentNullException("key");
-            if (iv == null || iv.Length <= 0)
-                throw new ArgumentNullException("iV");
             byte[] encrypted;
 
-            // Create an Aes object
-            // with the specified key and IV.
-            using (Aes aesAlg = Aes.Create())
+            // Create an encryptor to perform the stream transform.
+            ICryptoTransform encryptor = Algorithim.CreateEncryptor(Algorithim.Key, Algorithim.IV);
+
+            // Create the streams used for encryption.
+            using (MemoryStream msEncrypt = new MemoryStream())
             {
-                aesAlg.Key = key;
-                aesAlg.IV = iv;
-
-                // Create an encryptor to perform the stream transform.
-                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-                // Create the streams used for encryption.
-                using (MemoryStream msEncrypt = new MemoryStream())
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
                 {
-                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
                     {
-                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-                        {
-                            //Write all data to the stream.
-                            swEncrypt.Write(plain);
-                        }
-                        encrypted = msEncrypt.ToArray();
+                        //Write all data to the stream.
+                        swEncrypt.Write(plain);
                     }
-                }
-            }
-
-            // Return the encrypted bytes from the memory stream.
-            return encrypted;
-        }
-        private static byte[] Encrypt(byte[] plain, byte[] key, byte[] iv)
-        {
-            // Check arguments.
-            if (plain == null || plain.Length <= 0)
-                throw new ArgumentNullException("plain");
-            if (key == null || key.Length <= 0)
-                throw new ArgumentNullException("key");
-            if (iv == null || iv.Length <= 0)
-                throw new ArgumentNullException("iV");
-            byte[] encrypted;
-
-            // Create an Aes object
-            // with the specified key and IV.
-            using (Aes aesAlg = Aes.Create())
-            {
-                aesAlg.Key = key;
-                aesAlg.IV = iv;
-
-                // Create an encryptor to perform the stream transform.
-                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-                // Create the streams used for encryption.
-                using (MemoryStream msEncrypt = new MemoryStream())
-                {
-                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                    {
-                        using (BinaryWriter swEncrypt = new BinaryWriter(csEncrypt))
-                        {
-                            //Write all data to the stream.
-                            swEncrypt.Write(plain);
-                        }
-                        encrypted = msEncrypt.ToArray();
-                    }
+                    encrypted = msEncrypt.ToArray();
                 }
             }
 
@@ -222,42 +209,86 @@ namespace CryptoCalc.Core
             return encrypted;
         }
 
-        private static void SetKeyAndIV(byte[] password, int keySize)
+        /// <summary>
+        /// Encrypt plain bytes
+        /// </summary>
+        /// <param name="plain">The plain bytes</param>
+        /// <returns>the encrypted bytes</returns>
+        private static byte[] Encrypt(byte[] plain)
         {
-            AesCrypt.KeySize = keySize;
-            byte[] hash = null;
-            byte[] key = null;
-            byte[] iv = new byte[16];
-            switch (keySize)
+            byte[] encrypted;
+
+            // Create an encryptor to perform the stream transform.
+            ICryptoTransform encryptor = Algorithim.CreateEncryptor(Algorithim.Key, Algorithim.IV);
+
+            // Create the streams used for encryption.
+            using (MemoryStream msEncrypt = new MemoryStream())
             {
-                case 128:
-                    key = new byte[16];
-                    hash = Hash.Compute(HashAlgorithim.SHA256, password);
-                    Array.Copy(hash, key, 16);
-                    Array.Copy(hash, key.Length, iv, 0, 16);
-                    AesCrypt.Key = key;
-                    AesCrypt.IV = iv;
-                    break;
-                case 192:
-                    key = new byte[24];
-                    hash = Hash.Compute(HashAlgorithim.SHA384, password);
-                    Array.Copy(hash, key, 24);
-                    Array.Copy(hash, key.Length, iv, 0, 16);
-                    AesCrypt.Key = key;
-                    AesCrypt.IV = iv;
-                    break;
-                case 256:
-                    key = new byte[32];
-                    hash = Hash.Compute(HashAlgorithim.SHA512, password);
-                    Array.Copy(hash, key, 32);
-                    Array.Copy(hash, key.Length, iv, 0, 16);
-                    AesCrypt.Key = key;
-                    AesCrypt.IV = iv;
-                    break;
-                default:
-                    Debugger.Break();
-                    break;
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (BinaryWriter swEncrypt = new BinaryWriter(csEncrypt))
+                    {
+                        //Write all data to the stream.
+                        swEncrypt.Write(plain);
+                    }
+                    encrypted = msEncrypt.ToArray();
+                }
             }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
+        /// <summary>
+        /// Takes in the users password and hashes it to create the secret key and iv for the set key size
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="keySize"></param>
+        private static void SetKeyAndIV(byte[] password)
+        {
+            byte[] key = new byte[Algorithim.KeySize / 8];
+            byte[] iv = new byte[Algorithim.BlockSize / 8];
+            List<byte> hash = new List<byte>();
+            int b = 0;
+            var bytes = Hash.Compute(HashAlgorithim.SHA512, password);
+            for(int i = 0; i < key.Length + iv.Length; i++)
+            {
+                if(!(b < bytes.Length))
+                {
+                    b = 0;
+                }
+                hash.Add(bytes[b]);
+                b++;
+            }
+            Array.Copy(hash.ToArray(), key, key.Length);
+            Array.Copy(hash.ToArray(), key.Length, iv, 0, iv.Length);
+            Algorithim.Key = key;
+            Algorithim.IV = iv;
+        }
+
+        /// <summary>
+        /// Verify that pre conditions are met before doing any cipher actions
+        /// </summary>
+        /// <param name="input">the plain or encrypted string / bytes</param>
+        private static void CheckCipherSetup(object input)
+        {
+            // Check arguments.
+            if (input == null)
+                throw new ArgumentNullException("plain");
+            if(input is string inputs)
+            {
+                if(inputs.Length == 0)
+                    throw new ArgumentNullException("plain");
+            }
+            if (input is byte[] inputb)
+            {
+                if (inputb.Length == 0)
+                    throw new ArgumentNullException("plain");
+            }
+            if (Algorithim.Key == null || Algorithim.Key.Length <= 0)
+                throw new ArgumentNullException("key");
+            if (Algorithim.IV == null || Algorithim.IV.Length <= 0)
+                throw new ArgumentNullException("iV");
         }
 
         #endregion
