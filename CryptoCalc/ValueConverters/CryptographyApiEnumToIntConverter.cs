e0f8ae662621e7dc7dc4e3ecfdb8c693a46cd1ea
danderle <danderle86@gmail.com>
2020-06-12 18:52:03 +0200

    added the CryptographyApiEnumToIntConverter.cs

---
+++ CryptoCalc/ValueConverters/CryptographyApiEnumToIntConverter.cs
@@ -0,0 +1,22 @@
+﻿using CryptoCalc.Core;
+using System;
+using System.Globalization;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Converts the <see cref="CryptographyApi"/>enum value to an int value
+    /// </summary>
+    public class CryptographyApiEnumToInt : BaseValueConverter<CryptographyApiEnumToInt>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((int)(CryptographyApi)value);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (CryptographyApi)value;
+        }
+    }
+}
