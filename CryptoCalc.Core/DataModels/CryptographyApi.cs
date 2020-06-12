c81e9f2999db2c401783c1a673049d252f23747d
danderle <danderle86@gmail.com>
2020-06-12 18:46:38 +0200

    created a CryptographyApi.cs enum

---
+++ CryptoCalc.Core/DataModels/CryptographyApi.cs
@@ -0,0 +1,18 @@
+﻿namespace CryptoCalc.Core
+{
+    /// <summary>
+    /// The available cryptography librarys
+    /// </summary>
+    public enum CryptographyApi
+    {
+        /// <summary>
+        /// Hash a file
+        /// </summary>
+        MSDN = 0,
+
+        /// <summary>
+        /// Hash a string
+        /// </summary>
+        BouncyCastle,
+    }
+}
