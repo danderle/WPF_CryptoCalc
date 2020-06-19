a18f494e0c9fe1b077666b25bb3034534b325776
danderle <danderle86@gmail.com>
2020-06-19 06:39:34 +0200

    added an asymmetric cipher page

---
+++ CryptoCalc/Pages/AsymmetricCipherPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using CryptoCalc.Core;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for AsymmetricCipherPage.xaml
+    /// </summary>
+    public partial class AsymmetricCipherPage : BasePage<AsymmetricCipherViewModel>
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public AsymmetricCipherPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Constructor with a specific view model
+        /// </summary>
+        public AsymmetricCipherPage(AsymmetricCipherViewModel specificViewModel) : base(specificViewModel)
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
