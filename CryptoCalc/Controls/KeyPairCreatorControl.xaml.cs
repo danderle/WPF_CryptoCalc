859b6b4f57adb994ca4198b651849b451eae3aec
danderle <danderle86@gmail.com>
2020-06-19 06:42:01 +0200

    added signature and key a pair creator control

---
+++ CryptoCalc/Controls/KeyPairCreatorControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for KeyPairCreatorControl.xaml
+    /// </summary>
+    public partial class KeyPairCreatorControl : UserControl
+    {
+        #region Constructor
+
+        public KeyPairCreatorControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
---
+++ CryptoCalc/Controls/SignatureInputControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for SignatureInputControl.xaml
+    /// </summary>
+    public partial class SignatureInputControl : UserControl
+    {
+        #region Constructor
+
+        public SignatureInputControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
