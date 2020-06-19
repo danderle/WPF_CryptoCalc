2aa90d5030181423055af096c6da8e629d519ab4
danderle <danderle86@gmail.com>
2020-06-19 06:38:01 +0200

    added the asymmetric msdn ciphers enum

---
+++ CryptoCalc.Core/DataModels/AsymmetricMsdnCiphers.cs
@@ -0,0 +1,13 @@
+﻿namespace CryptoCalc.Core
+{
+    /// <summary>
+    /// Represents all the available asymmetric cipher algorithims
+    /// </summary>
+    public enum AsymmetricMsdnCiphers
+    {
+        DSA = 0,
+        ECDifiieHellman,
+        ECDsa,
+        RSA,
+    }
+}
