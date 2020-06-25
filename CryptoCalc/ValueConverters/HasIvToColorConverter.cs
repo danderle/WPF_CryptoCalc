2c39082723b55d261383fb7e0c310f2f8a89e379
danderle <danderle86@gmail.com>
2020-06-25 07:18:50 +0200

    added value converter for changing the HasIv bool to a background color

---
+++ CryptoCalc/ValueConverters/HasIvToColorConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Media;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Converts a bool to a color
+    /// </summary>
+    public class HasIvToColor : BaseValueConverter<HasIvToColor>
+    {
+        public override object Convert(object value, Type targetType = null, object parameter = null, CultureInfo culture = null)
+        {
+            return (bool)value ? new SolidColorBrush(Color.FromRgb(0xff, 0xff, 0xff)) : new SolidColorBrush(Color.FromRgb(0xee, 0xee, 0xee));
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
