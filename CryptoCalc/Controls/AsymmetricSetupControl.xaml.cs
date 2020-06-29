0ce4ecf74b565c11fcea72e95b2182ac063e865a
danderle <danderle86@gmail.com>
2020-06-29 07:31:18 +0200

    added the AsymmetricSetupControl

---
+++ CryptoCalc/Controls/AsymmetricSetupControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for AsymmetricSetupControl.xaml
+    /// </summary>
+    public partial class AsymmetricSetupControl : UserControl
+    {
+        #region Constructor
+
+        public AsymmetricSetupControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
