a3280de34f405abdbba2d5e7f23ceaf2e6268ab8
danderle <danderle86@gmail.com>
2020-07-02 07:34:41 +0200

    added a DeriveKeyControl and styled it

---
+++ CryptoCalc/Controls/DeriveKeyControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for DeriveKeyControl.xaml
+    /// </summary>
+    public partial class DeriveKeyControl : UserControl
+    {
+        #region Constructor
+
+        public DeriveKeyControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
