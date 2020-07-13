8a852c7db26e9a49a9f05c6e55f40c3647567353
danderle <danderle86@gmail.com>
2020-07-13 07:52:31 +0200

    created 2 interfaces to extract the differences of using elliptical curve algorithims vs not using ec curves

---
+++ CryptoCalc.Core/Models/AsymmetricCiphers/Interfaces/IECAlgorithims.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CryptoCalc.Core
+{
+    /// <summary>
+    /// An interface for algortihims that use EC curves
+    /// </summary>
+    public interface IECAlgorithims : IAsymmetricCipher
+    {
+        /// <summary>
+        /// Ges a list ofr all available ec curves
+        /// </summary>
+        /// <returns>the list of all ec curves</returns>
+        public ObservableCollection<string> GetEcCurves();
+
+        /// <summary>
+        /// Create a key pair by using a given ec curve
+        /// </summary>
+        /// <param name="curveName">the curve to use for key creation</param>
+        public void CreateKeyPair(string curveName);
+    }
+}
---
+++ CryptoCalc.Core/Models/AsymmetricCiphers/Interfaces/INonECAlgorithims.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CryptoCalc.Core
+{
+    /// <summary>
+    /// An interface for algortihims that do not use EC curves
+    /// </summary>
+    public interface INonECAlgorithims : IAsymmetricCipher
+    {
+        /// <summary>
+        /// Return the available key sizes for the given algorithim
+        /// </summary>
+        /// <param name="selectedAlgorithim"></param>
+        /// <returns></returns>
+        public ObservableCollection<int> GetKeySizes();
+
+        /// <summary>
+        /// Create a key pair for according to the key size
+        /// </summary>
+        /// <param name="keySize">the key size in bits</param>
+        public void CreateKeyPair(int keySize);
+    }
+}
