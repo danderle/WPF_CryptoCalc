36011a8aacdeb223bd15f962d5401c3b43c217e9
danderle <danderle86@gmail.com>
2020-07-17 08:16:33 +0200

    added an enum for selecting ec curve providers

---
+++ CryptoCalc.Core/DataModels/EcCurveProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace CryptoCalc.Core
+{
+    /// <summary>
+    /// Represents all the available EC curve providers
+    /// </summary>
+    public enum EcCurveProvider
+    {
+        SEC = 0,
+        NIST,
+        TELETRUST,
+        ANSSI,
+        GOST3410,
+        GM,
+    }
+}
