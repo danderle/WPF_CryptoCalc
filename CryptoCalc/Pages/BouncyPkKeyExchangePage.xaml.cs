11db5d7511e18c432623762df0aa88a009df8922
danderle <danderle86@gmail.com>
2020-07-31 08:23:15 +0200

    created the BouncyPkKeyExchangePage

---
+++ CryptoCalc/Pages/BouncyPkKeyExchangePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using CryptoCalc.Core;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for BouncyPkKeyExchangePage.xaml
+    /// </summary>
+    public partial class BouncyPkKeyExchangePage : BasePage<AsymmetricViewModel>
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BouncyPkKeyExchangePage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Constructor with a specific view model
+        /// </summary>
+        public BouncyPkKeyExchangePage(AsymmetricViewModel specificViewModel) : base(specificViewModel)
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
