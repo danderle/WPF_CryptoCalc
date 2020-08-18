cd48949f840f443df7d968bd3731a560012a1387
danderle <danderle86@gmail.com>
2020-08-18 20:44:03 +0200

    removed not needed "using" declarations

--- CryptoCalc.Core/ViewModels/HashPages/DataFormatViewModel.cs
+++ CryptoCalc.Core/ViewModels/HashPages/DataFormatViewModel.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Windows;
+using System.IO;
 using System.Linq;
 using System.Windows.Input;
-using System.IO;
 
 namespace CryptoCalc.Core
 {
