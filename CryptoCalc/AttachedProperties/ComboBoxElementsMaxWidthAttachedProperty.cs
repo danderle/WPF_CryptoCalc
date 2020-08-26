4088d35d36db707336d22fe4e365b6cef15b29ec
danderle <danderle86@gmail.com>
2020-08-26 07:23:59 +0200

    added comments to ComboBoxElementsMaxWidthAttachedProperty.cs

--- CryptoCalc/AttachedProperties/ComboBoxElementsMaxWidthAttachedProperty.cs
+++ CryptoCalc/AttachedProperties/ComboBoxElementsMaxWidthAttachedProperty.cs
@@ -9,38 +9,67 @@ namespace CryptoCalc
     /// <typeparam name="Parent"></typeparam>
     public class ComboBoxElementsMaxWidthProperty : BaseAttachedProperty<ComboBoxElementsMaxWidthProperty, bool>
     {
-
+        /// <summary>
+        /// Fires when value is set
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
         {
-            if (!(sender is ComboBox control))
+            //make sure we have a comboBox
+            if (!(sender is ComboBox comboBox))
             {
                 return;
             }
 
             //Adjust size when loaded
-            control.Loaded += Control_Loaded;
+            comboBox.Loaded += ComboBox_Loaded;
         }
 
-
-        private void Control_Loaded(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Fires when the comboBox is loaded
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComboBox_Loaded(object sender, RoutedEventArgs e)
         {
-            if (!(sender is ComboBox control))
+            //verify that we have a combobox
+            if (!(sender is ComboBox comboBox))
             {
                 return;
             }
+
+            // Ignore any non ComboBoxes
+            if (!(comboBox is ComboBox))
+                return;
+
+            //the maximum length of the items inside the combobox
             double max = 0;
-            foreach (var item in control.Items)
+
+            //cycle through the combobox items
+            foreach (var item in comboBox.Items)
             {
+                //Create a label for measuring the item text
                 Label label = new Label();
+
+                //set the combobox item as the label content 
                 label.Content = item.ToString();
+
+                //measure the text length
                 label.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+
+                //arranges the uiElement size
                 label.Arrange(new Rect(label.DesiredSize));
+
+                //get the width of the label
                 double width = label.ActualWidth;
+
+                //save the maximum item width
                 max = width > max ? width : max;
             }
 
             //TODO figure out how to get the size of dropdown arrow or size when empty
-            control.Width = max + 20;
+            comboBox.Width = max + 20;
         }
     }
 }
