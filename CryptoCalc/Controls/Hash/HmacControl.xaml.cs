11cc7ae2c72fac937ccd5a91e80a3b86bcfafe62
danderle <danderle86@gmail.com>
2020-08-31 08:53:22 +0200

    Created the HmacControl, HmacViewModel.cs and HmacDesignModel.cs

---
+++ CryptoCalc/Controls/Hash/HmacControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for HmacControl.xaml
+    /// </summary>
+    public partial class HmacControl : UserControl
+    {
+        #region Dependency Properties
+
+        /// <summary>
+        /// Dependency property for determining if the hmac checkbox is selected
+        /// </summary>
+        public bool HmacSelected
+        {
+            get => (bool)GetValue(HmacSelectedProperty);
+            set => SetValue(HmacSelectedProperty, value);
+        }
+
+        /// <summary>
+        /// Registers <see cref="HmacSelected"/> as a dependency property
+        /// </summary>
+        public static readonly DependencyProperty HmacSelectedProperty =
+            DependencyProperty.Register(nameof(HmacSelected), typeof(bool), typeof(HmacControl), new PropertyMetadata(false));
+
+        #endregion
+
+        #region Constructor
+
+        public HmacControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
