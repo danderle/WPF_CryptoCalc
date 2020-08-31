0ed5c1012ed32e0b0804680a8be749d7201cbc94
danderle <danderle86@gmail.com>
2020-08-31 08:58:34 +0200

    created  the asymmetric AsymmetricDesignModel.cs and implemented it

---
+++ CryptoCalc.Core/ViewModels/AsymmetricCiphers/Design/AsymmetricDesignModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CryptoCalc.Core
+{
+    /// <summary>
+    /// The design time model for the asymmetric cipher page
+    /// </summary>
+    public class AsymmetricDesignModel : AsymmetricViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static AsymmetricDesignModel Instance => new AsymmetricDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public AsymmetricDesignModel()
+        {
+            
+        }
+
+        #endregion
+    }
+}
