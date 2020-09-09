68a81308eb8110998bd9df08805d7809e92d49a1
danderle <danderle86@gmail.com>
2020-09-09 07:19:45 +0200

    made a custom ComboBoxWidth property and implemented it

--- CryptoCalc/Controls/Shared/DataInputControl.xaml.cs
+++ CryptoCalc/Controls/Shared/DataInputControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace CryptoCalc
 {
@@ -7,6 +8,25 @@ namespace CryptoCalc
     /// </summary>
     public partial class DataInputControl : UserControl
     {
+        #region Dependency Properties
+
+        /// <summary>
+        /// Dependency property for determining if the maxWidth of any comboBoxes
+        /// </summary>
+        public double ComboBoxMaxWidth
+        {
+            get => (double)GetValue(ComboBoxMaxWidthProperty);
+            set => SetValue(ComboBoxMaxWidthProperty, value);
+        }
+
+        /// <summary>
+        /// Registers <see cref="ComboBoxMaxWidth"/> as a dependency property
+        /// </summary>
+        public static readonly DependencyProperty ComboBoxMaxWidthProperty =
+            DependencyProperty.Register(nameof(ComboBoxMaxWidth), typeof(double), typeof(DataInputControl), new PropertyMetadata(0.0));
+
+        #endregion
+
         #region Constructor
 
         public DataInputControl()
