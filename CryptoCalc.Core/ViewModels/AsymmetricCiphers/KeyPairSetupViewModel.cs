50cf270931857268bad7fdc22249e513ea951be6
danderle <danderle86@gmail.com>
2020-09-11 09:55:41 +0200

    added new functions, properties and comments to KeyPairSetupViewModel.cs. Some were moved from the AsymmetricViewModel.cs.

--- CryptoCalc.Core/ViewModels/AsymmetricCiphers/KeyPairSetupViewModel.cs
+++ CryptoCalc.Core/ViewModels/AsymmetricCiphers/KeyPairSetupViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
 using System.Windows.Input;
 
 namespace CryptoCalc.Core
@@ -12,6 +13,8 @@ namespace CryptoCalc.Core
     public class KeyPairSetupViewModel : BaseViewModel
     {
         #region Private fields
+
+        public string pKeyPath = @"C:\Users\beach\Desktop\Encryption tests";
 
         #endregion
 
@@ -26,6 +29,36 @@ namespace CryptoCalc.Core
         /// Flag to know if the selected algorithim uses key size for creation
         /// </summary>
         public bool UsesKeySize { get; set; }
+
+        /// <summary>
+        /// Flag to let us know if the Directory for saving the keys in exist
+        /// </summary>
+        public bool KeyDirectoryPathExists => Directory.Exists(KeyDirectoryPath);
+
+        /// <summary>
+        /// Flag to let us know if the private key file exists
+        /// </summary>
+        public bool PrivateKeyFilePathExists => File.Exists(PrivateKeyFilePath);
+
+        /// <summary>
+        /// Flag to let us know if the public key path exists
+        /// </summary>
+        public bool PublicKeyFilePathExists => File.Exists(PublicKeyFilePath);
+
+        /// <summary>
+        /// Flag to let us know if the Private key is loaded
+        /// </summary>
+        public bool PrivateKeyLoaded { get; set; }
+
+        /// <summary>
+        /// Flag to let us know if the public key is loaded
+        /// </summary>
+        public bool PublicKeyLoaded { get; set; }
+
+        /// <summary>
+        /// Flag to let us know if any key exist
+        /// </summary>
+        public bool KeysExist => PrivateKeyFilePathExists || PublicKeyFilePathExists;
 
         /// <summary>
         /// The currently selected key size index
@@ -48,14 +81,19 @@ namespace CryptoCalc.Core
         public int SelectedAlgorithimIndex { get; set; } = 0;
 
         /// <summary>
-        /// The private key as a hex string
+        /// The dirtectory path to create a new key pair in
         /// </summary>
-        public string PrivateKey { get; set; } = string.Empty;
+        public string KeyDirectoryPath { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
         /// <summary>
-        /// The public key as a hex string
+        /// The path to the private key file
         /// </summary>
-        public string PublicKey { get; set; } = string.Empty;
+        public string PrivateKeyFilePath { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The file path to the public key
+        /// </summary>
+        public string PublicKeyFilePath { get; set; } = string.Empty;
 
         /// <summary>
         /// The Public key operation to do
@@ -71,6 +109,28 @@ namespace CryptoCalc.Core
         /// The selectred crypto library
         /// </summary>
         public CryptographyApi Api { get; set; }
+
+        /// <summary>
+        /// A delegate event handler when a key is loaded
+        /// </summary>
+        /// <param name="obj">the object from which event is triggered</param>
+        /// <param name="args">The event arguments</param>
+        public delegate void KeysLoadedEventHandler(object obj, EventArgs args);
+
+        /// <summary>
+        /// The data changed event
+        /// </summary>
+        public event KeysLoadedEventHandler KeysLoaded;
+
+        /// <summary>
+        /// The private key
+        /// </summary>
+        public byte[] PrivateKey { get; set; }
+
+        /// <summary>
+        /// The public key
+        /// </summary>
+        public byte[] PublicKey { get; set; }
 
         /// <summary>
         /// The list off all symmetric algorithims
@@ -111,6 +171,15 @@ namespace CryptoCalc.Core
         /// </summary>
         public ICommand CreateKeyPairCommand { get; set; }
 
+        /// <summary>
+        /// The command to load a key/s from given key paths
+        /// </summary>
+        public ICommand LoadKeyCommand { get; set; }
+
+        /// <summary>
+        /// The command to delete keys
+        /// </summary>
+        public ICommand DeleteKeyCommand { get; set; }
 
         #endregion
 
@@ -131,6 +200,8 @@ namespace CryptoCalc.Core
             InitializeLists();
 
             ChangedAlgorithim();
+
+            KeyDirectoryPath = pKeyPath;
         }
 
         /// <summary>
@@ -150,6 +221,8 @@ namespace CryptoCalc.Core
             InitializeLists();
 
             ChangedAlgorithim();
+
+            KeyDirectoryPath = pKeyPath;
         }
 
         #endregion
@@ -171,15 +244,23 @@ namespace CryptoCalc.Core
         /// </summary>
         private void ChangedAlgorithim()
         {
+            //Find out which api is beeing used
             switch (Api)
             {
+                //MSDN API
                 case CryptographyApi.MSDN:
+                    //Get the selected cipher algorithim
                     SelectedCipher = IAsymmetricCipher.GetMsdnCipher(Algorithims[SelectedAlgorithimIndex]);
                     break;
+
+                //Bouncy castle api
                 case CryptographyApi.BouncyCastle:
+                    //Get the selected cipher algorithim
                     SelectedCipher = IAsymmetricCipher.GetBouncyCipher(Algorithims[SelectedAlgorithimIndex]);
                     break;
             }
+
+            //True if the cipher uses ec curves
             if (SelectedCipher is IECAlgorithims ecCipher)
             {
                 UsesKeySize = false;
@@ -192,6 +273,8 @@ namespace CryptoCalc.Core
                     EcCurveIndex = 0;
                 }
             }
+
+            //true if the cipher algorithims doesnt use ec curves
             else if (SelectedCipher is INonECAlgorithims nonEcCipher)
             {
                 UsesEcCurves = false;
@@ -206,6 +289,7 @@ namespace CryptoCalc.Core
         /// </summary>
         private void CreateKeyPair()
         {
+            //True if the cipher uses ec curves
             if (SelectedCipher is IECAlgorithims ecCipher)
             {
                 if (EcCurves.Count != 0)
@@ -213,6 +297,7 @@ namespace CryptoCalc.Core
                     ecCipher.CreateKeyPair(EcCurves[EcCurveIndex]);
                 }
             }
+            //true if the cipher algorithims doesnt use ec curves
             else if (SelectedCipher is INonECAlgorithims nonEcCipher)
             {
                 if (KeySizes.Count != 0)
@@ -224,11 +309,103 @@ namespace CryptoCalc.Core
             {
                 return;
             }
-            PrivateKey = ByteConvert.BytesToHexString(SelectedCipher.GetPrivateKey());
-            PublicKey = ByteConvert.BytesToHexString(SelectedCipher.GetPublicKey());
+
+            //Create the file paths
+            PrivateKeyFilePath = $"{KeyDirectoryPath}\\PrivateKey.bin";
+            PublicKeyFilePath = $"{KeyDirectoryPath}\\PublicKey.bin";
+
+            //Write keys to the files
+            File.WriteAllBytes(PrivateKeyFilePath, SelectedCipher.GetPrivateKey());
+            File.WriteAllBytes(PublicKeyFilePath, SelectedCipher.GetPublicKey());
+
+            //Update the affected properties
+            OnPropertyChanged(nameof(PrivateKeyFilePathExists));
+            OnPropertyChanged(nameof(PublicKeyFilePathExists));
+            OnPropertyChanged(nameof(KeysExist));
+
+            //Load the keys ready to use
+            LoadKey();
+        }
+
+        /// <summary>
+        /// The command method to load a key/s
+        /// </summary>
+        private void LoadKey()
+        {
+            if (PrivateKeyFilePathExists)
+            {
+                PrivateKey = File.ReadAllBytes(PrivateKeyFilePath);
+                PrivateKeyLoaded = true;
+            }
+            if (PublicKeyFilePathExists)
+            {
+                PublicKey = File.ReadAllBytes(PublicKeyFilePath);
+                PublicKeyLoaded = true;
+            }
+            OnKeyLoad();
+        }
+
+        /// <summary>
+        /// the command method to delete a key pair
+        /// </summary>
+        private void DeleteKey()
+        {
+            if (PrivateKeyFilePathExists)
+            {
+                File.Delete(PrivateKeyFilePath);
+                PrivateKeyFilePath = string.Empty;
+                PrivateKeyLoaded = false;
+            }
+            if (PublicKeyFilePathExists)
+            {
+                File.Delete(PublicKeyFilePath);
+                PublicKeyFilePath = string.Empty;
+                PublicKeyLoaded = false;
+            }
+            OnKeyLoad();
         }
 
         #endregion
+
+        /// <summary>
+        /// Encrypt plain bytes
+        /// </summary>
+        /// <param name="plainBytes">The plain bytes to encrypt</param>
+        /// <returns>The encrypted bytes</returns>
+        public byte[] Encrypt(byte[] plainBytes)
+        {
+            return ((IAsymmetricEncryption)SelectedCipher).EncryptBytes(PublicKey, plainBytes);
+        }
+
+        /// <summary>
+        /// Encrypt plain text
+        /// </summary>
+        /// <param name="plainText">The plain text to encrypt</param>
+        /// <returns>The encrypted bytes</returns>
+        public byte[] Encrypt(string plainText)
+        {
+            return ((IAsymmetricEncryption)SelectedCipher).EncryptText(PublicKey, plainText);
+        }
+
+        /// <summary>
+        /// Decrypt to plain bytes
+        /// </summary>
+        /// <param name="encrypted">encrypted bytes</param>
+        /// <returns>Plain bytes</returns>
+        public byte[] Decrypt(byte[] encrypted)
+        {
+            return ((IAsymmetricEncryption)SelectedCipher).DecryptToBytes(PrivateKey, encrypted);
+        }
+
+        /// <summary>
+        /// Decrypt to text
+        /// </summary>
+        /// <param name="encrypted">encrypted bytes</param>
+        /// <returns>plain text</returns>
+        public string DecryptToText(byte[] encrypted)
+        {
+            return ((IAsymmetricEncryption)SelectedCipher).DecryptToText(PrivateKey, encrypted);
+        }
 
         #region Private Methods
 
@@ -259,6 +436,16 @@ namespace CryptoCalc.Core
             ChangedAlgorithimCommand = new RelayCommand(ChangedAlgorithim);
             CreateKeyPairCommand = new RelayCommand(CreateKeyPair);
             ChangedProviderCommand = new RelayCommand(ChangedProvider);
+            LoadKeyCommand = new RelayCommand(LoadKey);
+            DeleteKeyCommand = new RelayCommand(DeleteKey);
+        }
+
+        /// <summary>
+        /// Executes the Keys loaded event
+        /// </summary>
+        private void OnKeyLoad()
+        {
+            KeysLoaded?.Invoke(this, EventArgs.Empty);
         }
         #endregion
     }
