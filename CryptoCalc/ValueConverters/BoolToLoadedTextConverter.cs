b82182846d9ab3a947525bfc2b7395dea18f458d
danderle <danderle86@gmail.com>
2020-09-11 09:58:34 +0200

    Created the BoolToLoadedTextConverter.cs

---
+++ CryptoCalc/ValueConverters/BoolToLoadedTextConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Converts a bool to a loaded text
+    /// </summary>
+    public class BoolToLoadedText : BaseValueConverter<BoolToLoadedText>
+    {
+        public override object Convert(object value, Type targetType = null, object parameter = null, CultureInfo culture = null)
+        {
+            return (bool)value ? " --> LOADED" : string.Empty;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
