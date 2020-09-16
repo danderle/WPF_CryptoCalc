e8386f765e9e06ddb70f64fcfec9fb3145a18c5f
danderle <danderle86@gmail.com>
2020-09-16 07:28:01 +0200

    created the KeyExchangeSetupControl

---
+++ CryptoCalc/Controls/Asymmetric/KeyExchangeSetupControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Interaction logic for KeyExchangeSetupControl.xaml
+    /// </summary>
+    public partial class KeyExchangeSetupControl : UserControl
+    {
+        #region Constructor
+
+        public KeyExchangeSetupControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
