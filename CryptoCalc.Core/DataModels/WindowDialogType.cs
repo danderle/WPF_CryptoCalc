afbce66d385b18fb9c9a41fc7f39d5dd50f99b67
danderle <danderle86@gmail.com>
2020-10-05 11:09:36 +0200

    Created the Data Model WindowDialogType.cs enum to determine the type of Popup window dialog

---
+++ CryptoCalc.Core/DataModels/WindowDialogType.cs
@@ -0,0 +1,23 @@
+﻿namespace CryptoCalc.Core
+{
+    /// <summary>
+    /// The type of Popup window dialog
+    /// </summary>
+    public enum WindowDialogType
+    {
+        /// <summary>
+        /// The folder browser dialog
+        /// </summary>
+        FolderBrowser = 0,
+
+        /// <summary>
+        /// A warning dialog
+        /// </summary>
+        Warning,
+
+        /// <summary>
+        /// An error dialog
+        /// </summary>
+        Error,
+    }
+}
