3160edb953d49d84f27db062ccbc0e583a224d7b
danderle <danderle86@gmail.com>
2020-10-06 11:07:58 +0200

    created a static class for displaying different dialogs

---
+++ CryptoCalc.Core/ViewModels/Dialogs/Dialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoCalc.Core
+{
+    /// <summary>
+    /// static class for displaying different dialogs
+    /// </summary>
+    public static class Dialog
+    {
+        /// <summary>
+        /// Static constructor
+        /// </summary>
+        static Dialog()
+        {
+        }
+
+        /// <summary>
+        /// The command method to show an error dialog window and displays the exception message to the user
+        /// <param name="title">the title of the pop up dialog</param>
+        /// <param name="dialogType">The type of <see cref="WindowDialogType"/></param>
+        /// </summary>
+        public static async void OpenErrorMessageBoxAsync(Exception exception, string title, WindowDialogType dialogType)
+        {
+            //Opens a pop up window folder browser dialog
+            await Ioc.UI.ShowMessage(new MessageBoxDialogViewModel
+            {
+                Title = title,
+                Message = exception.Message,
+                OkText = "Continue",
+                DialogType = dialogType
+            });
+        }
+    }
+}
