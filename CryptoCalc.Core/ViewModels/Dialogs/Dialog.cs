140ddf45ea976e009f3ed25a66ce55ca1f353287
danderle <danderle86@gmail.com>
2020-10-06 11:19:40 +0200

    added functions for opening a help message box and a folder browser dialog

--- CryptoCalc.Core/ViewModels/Dialogs/Dialog.cs
+++ CryptoCalc.Core/ViewModels/Dialogs/Dialog.cs
@@ -17,19 +17,42 @@ namespace CryptoCalc.Core
         }
 
         /// <summary>
-        /// The command method to show an error dialog window and displays the exception message to the user
+        /// Opens an error message box
         /// <param name="title">the title of the pop up dialog</param>
         /// <param name="dialogType">The type of <see cref="WindowDialogType"/></param>
         /// </summary>
         public static async void OpenErrorMessageBoxAsync(Exception exception, string title, WindowDialogType dialogType)
         {
-            //Opens a pop up window folder browser dialog
+            //Opens a pop up message box dialog
             await Ioc.UI.ShowMessage(new MessageBoxDialogViewModel
             {
                 Title = title,
                 Message = exception.Message,
                 OkText = "Continue",
                 DialogType = dialogType
+            });
+        }
+
+        /// <summary>
+        /// Opens a folder browser dialog
+        /// </summary>
+        public static async void OpenFolderBrowserAsync()
+        { 
+            //Opens a pop up window folder browser dialog
+            await Ioc.UI.ShowFolderDialog(new FolderBrowserDialogViewModel());
+        }
+
+        /// <summary>
+        /// Opens a help message box dialog
+        /// </summary>
+        public static async void OpenHelpMessageBoxAsync()
+        {
+            //TODO Help dialog window
+            await Ioc.UI.ShowMessage(new MessageBoxDialogViewModel
+            {
+                Message = "Hello this is a pop up message",
+                Title = "First dialog message",
+                OkText = "Press ok to continue",
             });
         }
     }
