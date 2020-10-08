d6b515dcf898ef0de4ef4ab664ccf4cd4325389f
danderle <danderle86@gmail.com>
2020-10-08 12:36:49 +0200

    created the DataFormatsToPlainDataToolTip.cs and implemented in the CipherSetupControl.xaml

---
+++ CryptoCalc/ValueConverters/DataFormatsToPlainDataToolTip.cs
@@ -0,0 +1,34 @@
+﻿using CryptoCalc.Core;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace CryptoCalc
+{
+    /// <summary>
+    /// Converts a <see cref="Format"/> to a tool tip
+    /// </summary>
+    public class DataFormatsToPlainDataToolTip : BaseValueConverter<DataFormatsToPlainDataToolTip>
+    {
+        public override object Convert(object value, Type targetType = null, object parameter = null, CultureInfo culture = null)
+        {
+            switch((Format)value)
+            {
+                case Format.TextString:
+                    return "Enter a text";
+                case Format.HexString:
+                    return "Enter a hexadecimal value";
+                case Format.File:
+                    return "Enter a file path";
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
